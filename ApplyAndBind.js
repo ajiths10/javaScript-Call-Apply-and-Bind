7b22abaa8113e370d4c155678d4115f1f36ce4d0
Nuclear One <ajithsnair10@gmail.com>
2022-03-02 09:32:37 +0530

    updated value only

--- ApplyAndBind.js
+++ ApplyAndBind.js
@@ -14,9 +14,10 @@ console.log(Showage.call(obj));
 
 //apply
 var numb={num:10};
-
+var numb1={num:1}
 var numberfn = function(a,b,c){
     return this.num+a+b+c;
 }
 var arr=[10,20,30];
 console.log(numberfn.apply(numb,arr))
+console.log(numberfn.apply(numb1,arr))
