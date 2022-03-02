a91500edfade55a5590bb7edf10a9369744728f5
Nuclear One <ajithsnair10@gmail.com>
2022-03-02 09:48:39 +0530

    add bind

--- ApplyAndBind.js
+++ ApplyAndBind.js
@@ -1,23 +1,29 @@
-//call
-var obj={name:'Tom',
-        age: 23,
-        email:'Tom@email.com',
-};
+//Testing object creation
+var numb={num:10};
+var numb1={num:1}
 
+//call
 var Showage = function(){
     console.log(this.name);
     return this.age;
 };
-
 console.log(Showage.call(obj));
 
 
 //apply
-var numb={num:10};
-var numb1={num:1}
 var numberfn = function(a,b,c){
     return this.num+a+b+c;
 }
 var arr=[10,20,30];
 console.log(numberfn.apply(numb,arr))
 console.log(numberfn.apply(numb1,arr))
+
+//bind
+var bound=numberfn.bind(numb);
+console.log(bound(1,2,3));
+
+//object created for testing
+var obj={name:'Tom',
+        age: 23,
+        email:'Tom@email.com',
+};
